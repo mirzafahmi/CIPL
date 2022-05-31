05eae4794cf8023da0a054a36d2aab23f5466e72
mirza fahmi <mirzafahmi7@gmail.com>
2022-05-31 21:44:51 +0800

    1.01

--- CIPL.py
+++ CIPL.py
@@ -1,19 +1,49 @@
 import pandas as pd
-
 from PIL import Image, ImageDraw, ImageFont
+import openpyxl
+from win32com import client
+import win32api
+import re
+  
 
-print('Enter file name')
+data = pd.read_excel('CIPL.xlsx', sheet_name = ['template', 'PL', 'CI'])
+masterList = data["template"]
+packingList = data["PL"]
+commercialInvoice = data["CI"]
 
-#fileName = input()
 
-data = pd.read_excel('CIPL.xlsx')
+customerList = masterList.head(masterList.index.stop)
 
-nameList = data["TO"].tolist()
-#attnList = data["ATTENTION"].tolist()
-#addressList = data["ADDRESS"].tolist()
-#phoneNoList = data["PHONE NUMBER"].tolist()
-#CINoList = data["CI NUMBER"].tolist()
-#PLNoList = data["PL NUMBER"].tolist()
-#PORefNoList = data["PO REF NUMBER"].tolist()
+print(customerList)
 
-print(nameList)
+customerDetails = []
+
+for ind,row in customerList.iterrows():
+    f=[]
+    for z in row:
+        f.append(z)
+    customerDetails.append(f)
+print(customerDetails)
+
+plWorkbook=openpyxl.load_workbook('Book1.xlsx')
+plWorksheet= plWorkbook.get_sheet_by_name('PL')
+
+for details in customerDetails:
+    customerNames = details[0]
+    attention = details[1]
+    customerAddres = details[2]
+    customerPhone = details[3]
+    ciNo = details[4]
+    plNo = details[5]
+    poRef = details[6]
+
+    plWorksheet['D12']= customerNames
+
+    print(plWorksheet['D12'])
+    
+open('Book1.xlsx')
+
+  
+
+
+  
